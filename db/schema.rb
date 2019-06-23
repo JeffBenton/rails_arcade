fc76fe9ef63e27fcaf4f816f4d82d746e94abcb5
Jeff Benton <jeff@frogrock.net>
2019-06-23 12:09:46 -0400

    updated users and plays tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_035255) do
+ActiveRecord::Schema.define(version: 2019_06_23_160804) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 2019_06_23_035255) do
   create_table "plays", force: :cascade do |t|
     t.integer "user_id"
     t.integer "game_id"
-    t.integer "quantity"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "difficulty"
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_06_23_035255) do
     t.integer "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: true
   end
 
 end
