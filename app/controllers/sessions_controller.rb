0377b256446f1e7bba5df5e2d34a4c9d1bfc69cd
Jeff Benton <jeff@frogrock.net>
2019-06-28 01:50:23 -0400

    added login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(name: params[:name])
+    return redirect_to root_url if @user.nil? || !@user.authenticate(params[:password])
+
+    session[:user_id] = @user.id
+    redirect_to games_path
   end
 
   def destroy
+    session.delete :user_id
+    redirect_to root_url
   end
 end
