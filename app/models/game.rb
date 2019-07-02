f553cff5463c325b633b3489e250d3e2ccb59e46
Jeff Benton <jeff@frogrock.net>
2019-07-02 02:37:00 -0400

    added model validations

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,5 +3,8 @@ class Game < ApplicationRecord
   has_many :users, through: :plays
   belongs_to :manufacturer
 
+  validates :name, presence: true, uniqueness: true
+  validates :token_cost, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   accepts_nested_attributes_for :manufacturer, reject_if: :all_blank
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :plays
   has_many :games, through: :plays
   has_secure_password
+
+  validates :name, presence: true, uniqueness: true, length: { in: 2..20}
+  validates :password, presence: true
+
 end
