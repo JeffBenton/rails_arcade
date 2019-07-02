2a77f673d288d07e8aa10f03e948195889327469
Jeff Benton <jeff@frogrock.net>
2019-07-02 01:17:54 -0400

    added manufacturer functionality

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,25 +1,39 @@
 class ManufacturersController < ApplicationController
+  before_action :find_manufacturer, only: [:show, :edit, :update]
+
   def index
+    @manufacturers = Manufacturer.all
   end
 
   def show
   end
 
   def new
+    @manufacturer = Manufacturer.new
   end
 
   def create
     manufacturer = Manufacturer.create(manufacturer_params)
-    redirect_to root_url
+    redirect_to manufacturer
   end
 
   def edit
   end
 
   def update
+    @manufacturer.update(manufacturer_params)
+    if @manufacturer.save
+      redirect_to manufacturer_path(@manufacturer)
+    else
+      return redirect_to edit_manufacturer_path(@manufacturer)
+    end
   end
 
   private
+
+  def find_manufacturer
+    @manufacturer = Manufacturer.find_by(id: params[:id])
+  end
 
   def manufacturer_params
     params.require(:manufacturer).permit(:name)
